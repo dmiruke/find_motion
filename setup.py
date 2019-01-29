5c3539d8973b52a92011d168744d5ad109dfec32
aegilops <41705651+aegilops@users.noreply.github.com>
2019-01-29 20:49:33 +0000

    Added requirement

--- setup.py
+++ setup.py
@@ -3,9 +3,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = [
+    'progressbar2',
+    'mem_top',
+    'orderedset',
+    'opencv-python',
+    'imutils',
+    'numpy',
+    'pynput',
+]
+
 setuptools.setup(
     name="find_motion",
-    version="0.0.1",
+    version="0.0.2",
     author="Aegilops",
     author_email="41705651+aegilops@users.noreply.github.com",
     description="Processes video to detect motion, with tunable parameters",
@@ -13,14 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aegilops/find_motion",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'progressbar2',
-        'mem_top',
-        'orderedset',
-        'opencv-python',
-        'imutils',
-        'numpy',
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
